3982ca0cd040c663870fa64586f937fb1c4ed0c2
Daniel Barden <daniel.barden@gmail.com>
2013-05-06 19:22:44 +0200

    Initial Commit

---
+++ DoodleDrop/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DoodleDrop
+//
+//  Created by Daniel Barden on 5/6/13.
+//  Copyright Daniel Barden 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
